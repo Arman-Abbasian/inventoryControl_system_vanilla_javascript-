90a2ea3e7d56d32f217aac51ac4d17e23a37c735
Arman Abbasian <abasian.arman@gmail.com>
2022-09-18 12:16:04 +0430

    make AddNewProduct page

--- src/js/pages/AddNewProduct.js
+++ src/js/pages/AddNewProduct.js
@@ -1,10 +1,47 @@
-import  { visual } from "../visualPages/AddNewProductVisual.js";
-
+import { addNewProductvisual } from "../visualPages/AddNewProductVisual.js";
 const main=document.querySelector("main")
+
+
 class AddNewProduct{
     constructor(){
-        main.innerHTML=visual;
-        this.newProduct={productName:"",productSpecification:"",measurmentUnit:""}
+        main.innerHTML=addNewProductvisual;
+        this.newProduct={productName:"",productSpecification:"",measurementUnit:""};
+        //beacuse this add just by js to dom you can not add this two element at top of the this code
+        this.productName=document.querySelector("#productName");
+        this.productSpecification=document.querySelector("#productSpecification");
+        this.unit=document.getElementsByName("unit");
+        this.productName.addEventListener("change",(e)=>this.newProduct.productName=e.target.value);
+        this.productSpecification.addEventListener("change",(e)=>this.newProduct.productSpecification=e.target.value);
+       [...this.unit].forEach(item=>item.addEventListener("change",(e)=>{
+        [...this.unit].forEach(item=>item.removeAttribute("checked"));
+        item===e.target && item.setAttribute("checked",'');
+        this.newProduct.measurementUnit=item.value;
+    }));
+    this.addNewProduct=document.querySelector("#addNewProduct");
+        console.log(this.addNewProduct)
+        this.addNewProduct.addEventListener("click",(e)=>{
+            e.preventDefault();
+            console.log(this.newProduct);
+            this.productName.value="";
+            this.productSpecification.value="";
+            [...this.unit].forEach(item=>item.value="");
+        })
+
+      
+      
     };
+    changeHandler(e){
+        console.log(e.target.value)
+    };
+    
+
+
+
+
+
+
+
+
+
 }
 export default new AddNewProduct();
--- src/js/visualPages/AddNewProductVisual.js
+++ src/js/visualPages/AddNewProductVisual.js
@@ -1,18 +1,20 @@
 
-     export const  visual=`
-        <div class="p-2 drop-shadow-lg bg-gray-700 rounded-md">
+     export const addNewProductvisual=
+      `<div class="p-2 drop-shadow-lg bg-gray-700 rounded-md">
         <form class="flex flex-col justify-center items-center gap-y-4 p-1">
            <div class="flex flex-col justify-center items-start gap-2 w-2/4">
             <label for="productName" class="w-full">product name</label>
-            <input type="text" id="productName" class="rounded-md w-full"/>
+            <input type="text" " class="rounded-md w-full text-black" id="productName"/>
            </div>
            <div class="flex flex-col justify-center items-start gap-2 w-2/4">
-            <label for="productName" class="w-full">product specification</label>
-            <input type="text" id="productName" class="rounded-md w-full" />
+            <label for="productSpecification" class="w-full">product specification</label>
+            <input type="text" class="rounded-md w-full text-black" id="productSpecification"/>
            </div>
+           <label  class="w-2/4">unit of measurement</label>
            <div class="flex flex-col justify-center items-start gap-1 drop-shadow-md shadow-md p-2 rounded-md bg-slate-700 w-2/4">
+           
                 <div>
-                    <input type="radio" id="kg" name="unit" value="kg" class="cursor-pointer">
+                    <input type="radio" id="kg" name="unit" value="kg" class="cursor-pointe">
                     <label for="kg">kg</label><br>
                 </div>
                 
@@ -34,7 +36,7 @@
                 </div>
            </div>
 
-           <button class="px-4 py-2 bg-blue-600 rounded-md w-2/4 hover:bg-blue-500">Add</button>
+           <button class="px-4 py-2 bg-blue-600 rounded-md w-2/4 hover:bg-blue-500" id="addNewProduct">Add</button>
             
         </form>
     </div>
