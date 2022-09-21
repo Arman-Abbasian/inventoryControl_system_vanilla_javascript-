96a7364f624bcf8f818b60fc462e65abd0f9a883
Arman Abbasian <abasian.arman@gmail.com>
2022-09-21 13:35:50 +0430

    some changes

--- src/js/pages/Enter.js
+++ src/js/pages/Enter.js
@@ -5,8 +5,9 @@ class Enter{
         //make the content of this page
         main.innerHTML=enter;
         this.productOptions=[];
+        this.supplierOption=[];
         //inital container
-        this.enterData={selectedProductName:"",selectedSpecification:"",selectedDate:"",selectedNumber:null,selectedSupplier:"",selectedRecipent:"",selectedDelivered:[],transfree:""};
+        this.enterData={selectedProductName:"",selectedSpecification:"",selectedDate:"",selectedNumber:null,selectedSupplier:"",selectedDelivered:[],transfree:""};
         //get the element
         this.productNameInput=document.querySelector("#productNameInput");
         this.productNameDataList=document.querySelector("#productNameDataList");
@@ -25,11 +26,12 @@ class Enter{
         this.dateInput.addEventListener("change",(e)=>this.dateInputHandler(e));
         this.numberOfProductInput.addEventListener("change",(e)=>this.numberOfProductInputHandler(e));
         this.selectedSupplierSelectInput.addEventListener("change",(e)=>this.selectedSupplierSelectInputHandler(e));
+        this.selectedTransfreeInputSelect.addEventListener("change",(e)=>this.selectedTransfreeInputSelectHandler(e))
         this.addDataButton.addEventListener("click",(e)=>this.addDataButtonHandler(e));
-        this.fillProductOption()
-        this.fillPersonnelRecipentOptions();
-        this.makeselectedDeliveredCheckbox();
-        
+        this.fillProductOption();
+        this.fillSupplier();
+        this.makeselectedDeliveredCheckbox(); 
+        this.fillTransfreeOptions(); 
     }
 
     fillProductOption(){
@@ -49,17 +51,24 @@ class Enter{
         .catch(err=>console.log(err))
     };
 
-    fillPersonnelRecipentOptions(){
-        let personnelContainer="";
-        axios.get("http://localhost:4000/personnel")
+    fillSupplier(){
+        axios.get("http://localhost:4000/suppliers")
         .then(res=>{
-            const data=res.data;
-            data.forEach(item => {
-                personnelContainer+=`<option value='${item.name}'>${item.name}</option>`
+            const supplierOptions=res.data.map(item=>item.name);
+            const uniqueSupplierOptions = supplierOptions.filter((c, index) => {
+                return supplierOptions.indexOf(c) === index;
             });
-            this.selectedSupplierDataList.innerHTML=personnelContainer;
+            this.supplierOptions=uniqueSupplierOptions;
+            let supplierOptionsContainer="";
+            this.supplierOptions.forEach(item=>{
+                supplierOptionsContainer+=`<option value='${item}'>`
+            });
+            this.selectedSupplierDataList.innerHTML=supplierOptionsContainer;
+            console.log(this.selectedSupplierDataList)
+            console.log(this.supplierOptions)
         })
         .catch(err=>console.log(err))
+
     };
 
     makeselectedDeliveredCheckbox(){
@@ -88,8 +97,21 @@ class Enter{
             })
         })
         .catch(err=>console.log(err));
-    }
+    };
 
+    fillTransfreeOptions(){
+        axios.get("http://localhost:4000/transfree")
+        .then(res=>{
+            const data=res.data;
+            let transfreeContainer="";
+            data.forEach(item => {
+                transfreeContainer+=`<option value='${item.name}'>${item.name}</option>`
+            });
+            this.selectedTransfreeDataList.innerHTML= transfreeContainer;
+            console.log(this.selectedTransfreeDataList);
+        })
+        .catch(err=>console.log(err))
+    };
 
 
     productNameInputHandler(e){
@@ -109,7 +131,6 @@ class Enter{
             })
             .catch(err=>console.log(err))
         };
-
     };
 
     productSpecificationSelectHandler(e){
@@ -125,15 +146,20 @@ class Enter{
         this.enterData.selectedNumber=parseInt(e.target.value);
     };
 
-    selectedRecipentSelectInputHandler(e){
-        this.enterData.selectedRecipent=e.target.value;
+    selectedSupplierSelectInputHandler(e){
+        this.enterData.selectedSupplier=e.target.value;
     };
+
+    selectedTransfreeInputSelectHandler(e){
+        this.enterData.transfree=e.target.value;
+    };
+    
     addDataButtonHandler(e){
         e.preventDefault();
-        axios.post("http://localhost:4000/exit",this.enterData);
-        e.preventDefault();
+        axios.post("http://localhost:4000/enter",this.enterData);
+        console.log(this.enterData)
         this.enterData={selectedProductName:"",selectedSpecification:"",selectedDate:"",selectedNumber:null,selectedRecipent:"",selectedDelivered:[]};
 
-    }
+    };
 };
 export default Enter;
